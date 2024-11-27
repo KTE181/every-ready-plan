13bd6541462c3e4ed78e816056f42856dfa44a52
abc <abc@yaho.com>
2024-11-27 17:52:41 +0900

    휴가등록 구현

---
+++ springworkspace/semi/src/main/java/com/kh/semi/hr/vacation/service/VacationService.java
@@ -0,0 +1,32 @@
+package com.kh.semi.hr.vacation.service;
+
+import com.kh.semi.hr.employee.vo.EmployeeVo;
+import com.kh.semi.hr.vacation.mapper.VacationMapper;
+import com.kh.semi.hr.vacation.vo.VacationVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class VacationService {
+
+    private final VacationMapper mapper;
+
+    public List<VacationVo> list() {
+        return mapper.list();
+    }
+
+    public int insert(VacationVo vo) {
+        return mapper.insert(vo);
+    }
+
+    public List<EmployeeVo> empVoList( ) {
+        return mapper.empVoList();
+    }
+
+    public EmployeeVo selectvolist(String empNo) {
+        return mapper.selectvolist(empNo);
+    }
+}
--- springworkspace/semi/src/main/webapp/WEB-INF/views/common/header.jsp
+++ springworkspace/semi/src/main/webapp/WEB-INF/views/common/header.jsp
@@ -1,10 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 pageEncoding="UTF-8"%>
 
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EVERY READY PLAN</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="/css/common/header.css">
     <script defer src="/js/common/header.js"></script>
 </head>
