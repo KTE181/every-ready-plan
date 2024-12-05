e78fde58bf3b9c835587f52d607b0f96e02b654b
abc <abc@yaho.com>
2024-12-05 12:30:21 +0900

    사이드바 일반사원 ~~~  권한설정완료 admin 계정은 아직입니다~~~

--- springworkspace/semi/src/main/resources/static/js/common/sidebar.js
+++ springworkspace/semi/src/main/resources/static/js/common/sidebar.js
@@ -105,137 +105,164 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-function navi(){
+function navi() {
 
-//조직도 이동
-document.querySelector('.dropdown-menu li:nth-child(1)').addEventListener("click",()=>{
-  location.href="/organization";
-})
-//연혁도 이동
-document.querySelector('.dropdown-menu li:nth-child(2)').addEventListener("click",()=>{
-  location.href="/history";
-})
-//직원 검색
-document.querySelector('.dropdown-menu li:nth-child(3)').addEventListener("click",()=>{
-  location.href="/employee";
-})
+  //조직도 이동
+  document.querySelector('.dropdown-menu li:nth-child(1)').addEventListener("click", () => {
+    location.href = "/organization";
+  })
+  //연혁도 이동
+  document.querySelector('.dropdown-menu li:nth-child(2)').addEventListener("click", () => {
+    location.href = "/history";
+  })
+  //직원 검색
+  document.querySelector('.dropdown-menu li:nth-child(3)').addEventListener("click", () => {
+    location.href = "/employee";
+  })
 }
 
 
 
-function pvnavi(){
-//사이드바 마이페이지 이동
-document.querySelector('.dropdown-menu1 li:nth-child(1)').addEventListener("click",()=>{
-  location.href="/mypage";
-})
-//사이드바 개인휴가내역 이동
-document.querySelector('.dropdown-menu1 li:nth-child(2)').addEventListener("click",()=>{
-  location.href="/pv/vacation/list";
-})
+function pvnavi() {
+  //사이드바 마이페이지 이동
+  document.querySelector('.dropdown-menu1 li:nth-child(1)').addEventListener("click", () => {
+    location.href = "/mypage";
+  })
+  //사이드바 개인휴가내역 이동
+  document.querySelector('.dropdown-menu1 li:nth-child(2)').addEventListener("click", () => {
+    location.href = "/pv/vacation/list";
+  })
 }
 
-function hrnavi(){
-//초과근무 이동
-document.querySelector('.dropdown-menu2 li:nth-child(3)').addEventListener("click",()=>{
-  location.href="/api/hr/overtime/list";
-})
-//휴가 이동
-document.querySelector('.dropdown-menu2 li:nth-child(4)').addEventListener("click",()=>{
-  location.href="/api/hr/vacation/list";
-})
-//급여 이동
-document.querySelector('.dropdown-menu2 li:nth-child(5)').addEventListener("click",()=>{
-  location.href="/api/hr/salary/list";
-})
+function hrnavi() {
+  //초과근무 이동
+  document.querySelector('.dropdown-menu2 li:nth-child(3)').addEventListener("click", () => {
+    location.href = "/api/hr/overtime/list";
+  })
+  //휴가 이동
+  document.querySelector('.dropdown-menu2 li:nth-child(4)').addEventListener("click", () => {
+    location.href = "/api/hr/vacation/list";
+  })
+  //급여 이동
+  document.querySelector('.dropdown-menu2 li:nth-child(5)').addEventListener("click", () => {
+    location.href = "/api/hr/salary/list";
+  })
 }
 
-function qrnavi(){
-//상품관리 이동
-document.querySelector('.dropdown-menu3 li:nth-child(1)').addEventListener("click",()=>{
-  location.href="/qa/product/list";
-})
-//재고 현황 조회 이동
-document.querySelector('.dropdown-menu3 li:nth-child(2)').addEventListener("click",()=>{
-  location.href="/qa/productcnt/list";
-})
-//AS요청관리 이동
-document.querySelector('.dropdown-menu3 li:nth-child(4)').addEventListener("click",()=>{
-  location.href="/qa/asreq/list";
-})
-//담당자 관리 이동
-document.querySelector('.dropdown-menu3 li:nth-child(7)').addEventListener("click",()=>{
-  location.href="/qa/asemp/list";
-})
-//불량코드 관리 이동
-document.querySelector('.dropdown-menu3 li:nth-child(8)').addEventListener("click",()=>{
-  location.href="/qa/defectivecode/list";
-})
-}
-
-
-
-
-function finavi(){
-//거래처 관리 이동
-document.querySelector('.dropdown-menu4 li:nth-child(1)').addEventListener("click",()=>{
-  location.href="/finance/partner/list";
-})
-//회사계좌 관리 이동
-document.querySelector('.dropdown-menu4 li:nth-child(2)').addEventListener("click",()=>{
-  location.href="/finance/account/list";
-})
-
-//매출 관리 이동
-document.querySelector('.dropdown-menu4 li:nth-child(3)').addEventListener("click",()=>{
-  location.href="/finance/sale/list";
-})
-//매입 관리 이동
-document.querySelector('.dropdown-menu4 li:nth-child(4)').addEventListener("click",()=>{
-  location.href="/finance/purchase/list";
-})
-//경비 관리 이동
-document.querySelector('.dropdown-menu4 li:nth-child(5)').addEventListener("click",()=>{
-  location.href="/finance/expense/list";
-})
-//재무상태표 관리 이동
-document.querySelector('.dropdown-menu4 li:nth-child(7)').addEventListener("click",()=>{
-  location.href="/finance/bas/detail";
-})
-//손인계산서 관리 이동
-document.querySelector('.dropdown-menu4 li:nth-child(8)').addEventListener("click",()=>{
-  location.href="/finance/inc/detail";
-})
+function qrnavi() {
+  //상품관리 이동
+  document.querySelector('.dropdown-menu3 li:nth-child(1)').addEventListener("click", () => {
+    location.href = "/qa/product/list";
+  })
+  //재고 현황 조회 이동
+  document.querySelector('.dropdown-menu3 li:nth-child(2)').addEventListener("click", () => {
+    location.href = "/qa/productcnt/list";
+  })
+  //AS요청관리 이동
+  document.querySelector('.dropdown-menu3 li:nth-child(4)').addEventListener("click", () => {
+    location.href = "/qa/asreq/list";
+  })
+  //담당자 관리 이동
+  document.querySelector('.dropdown-menu3 li:nth-child(7)').addEventListener("click", () => {
+    location.href = "/qa/asemp/list";
+  })
+  //불량코드 관리 이동
+  document.querySelector('.dropdown-menu3 li:nth-child(8)').addEventListener("click", () => {
+    location.href = "/qa/defectivecode/list";
+  })
 }
 
 
-const insasidebar = document.querySelectorAll(".disabledinsa");
 
 
-// for (let i = 0; i < insasidebar.length; i++) {
-//   const e = insasidebar[i];
-//   e.addEventListener("click",(event)=>{
-//     alert("권한이 없습니다");
-//     console.log(event);
-//     event.preventDefault();
-//   });
-// }
+function finavi() {
+  //거래처 관리 이동
+  document.querySelector('.dropdown-menu4 li:nth-child(1)').addEventListener("click", () => {
+    location.href = "/finance/partner/list";
+  })
+  //회사계좌 관리 이동
+  document.querySelector('.dropdown-menu4 li:nth-child(2)').addEventListener("click", () => {
+    location.href = "/finance/account/list";
+  })
+
+  //매출 관리 이동
+  document.querySelector('.dropdown-menu4 li:nth-child(3)').addEventListener("click", () => {
+    location.href = "/finance/sale/list";
+  })
+  //매입 관리 이동
+  document.querySelector('.dropdown-menu4 li:nth-child(4)').addEventListener("click", () => {
+    location.href = "/finance/purchase/list";
+  })
+  //경비 관리 이동
+  document.querySelector('.dropdown-menu4 li:nth-child(5)').addEventListener("click", () => {
+    location.href = "/finance/expense/list";
+  })
+  //재무상태표 관리 이동
+  document.querySelector('.dropdown-menu4 li:nth-child(7)').addEventListener("click", () => {
+    location.href = "/finance/bas/detail";
+  })
+  //손인계산서 관리 이동
+  document.querySelector('.dropdown-menu4 li:nth-child(8)').addEventListener("click", () => {
+    location.href = "/finance/inc/detail";
+  })
+}
+
+
+const sidebar = document.querySelectorAll(".dropdown-menu .disabled");
+const pvsidebar = document.querySelectorAll(".dropdown-menu1 .disabled");
+const insasidebar = document.querySelectorAll(".dropdown-menu2 .disabled");
+const qasidebar = document.querySelectorAll(".dropdown-menu3 .disabled");
+const fisidebar = document.querySelectorAll(".dropdown-menu4 .disabled");
+function access(data) {
+  
+  
+  if (data != 1) {
+    for (let i = 0; i < insasidebar.length; i++) {
+      const e = insasidebar[i];
+
+      e.addEventListener("click", () => {
+        alert("권한이 없습니다");
+      });
+    }
+  }
+  if (data != 2) {
+    for (let i = 0; i < qasidebar.length; i++) {
+      const e = qasidebar[i];
+      e.addEventListener("click", () => {
+        alert("권한이 없습니다");
+      });
+    }
+  }
+  if (data != 3) {
+    for (let i = 0; i < fisidebar.length; i++) {
+      const e = fisidebar[i];
+      e.addEventListener("click", () => {
+        alert("권한이 없습니다");
+      });
+    }
+  }
+}
+
 
 function disabledinsadelete(data) {
 
-  if(data ==1){
+  if (data == 1) {
     navi();
     pvnavi();
     hrnavi();
+    access(1);
   }
-  if(data ==2){
+  if (data == 2) {
     navi();
     pvnavi();
     qrnavi();
+    access(2);
   }
-  if(data==3){
+  if (data == 3) {
     navi();
     pvnavi();
     finavi();
+    access(3);
   }
 
 }
